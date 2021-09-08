94402a6df883582971a85d36f5b515e1814f3e84
DOSkanya <65843170+DOSkanya@user.noreply.github.com>
2021-09-08 15:45:34 +0800

    texture

--- include/sphere.h
+++ include/sphere.h
@@ -14,6 +14,14 @@ public:
 	point3 center;
 	double radius;
 	shared_ptr<material> mat_ptr;
+private:
+	static void get_sphere_uv(const point3& p, double& u, double& v) {
+		auto theta = acos(-p.y());
+		auto phi = atan2(-p.z(), p.x()) + pi;
+
+		u = phi / (2 * pi);
+		v = theta / pi;
+	}
 };
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
@@ -38,6 +46,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 	rec.p = r.at(rec.t);
 	vec3 outward_normal = (rec.p - center) / radius;//Normalized
 	rec.set_face_normal(r, outward_normal);
+	get_sphere_uv(outward_normal, rec.u, rec.v);
 	rec.mat_ptr = mat_ptr;
 
 	return true;

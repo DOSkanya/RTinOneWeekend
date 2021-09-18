f413b6983588ebbc8438f465c7ccd258b753f4e7
DOSkanya <65843170+DOSkanya@user.noreply.github.com>
2021-09-18 15:17:06 +0800

    cornell box

--- src/main.cpp
+++ src/main.cpp
@@ -125,11 +125,29 @@ hittable_list simple_light() {
 	return objects;
 }
 
+hittable_list cornell_box() {
+	hittable_list objects;
+
+	auto red = make_shared<lambertian>(color(.65, .05, .05));
+	auto white = make_shared<lambertian>(color(.73, .73, .73));
+	auto green = make_shared<lambertian>(color(.12, .45, .15));
+	auto light = make_shared<lambertian>(color(15, 15, 15));
+
+	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+	objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+	return objects;
+}
+
 
 int main() {
 	//Image
-	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 400;
+	auto aspect_ratio = 16.0 / 9.0;
+	int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	int samples_per_pixel = 100;
 	const int max_depth = 50;
@@ -142,7 +160,7 @@ int main() {
 	auto aperture = 0.0;
 	color background(0, 0, 0);
 
-	switch (4) {
+	switch (6) {
 		case 1 :
 			world = random_scene();
 			background = color(0.70, 0.80, 1.00);
@@ -183,6 +201,17 @@ int main() {
 			lookfrom = point3(26, 3, 6);
 			lookat = point3(0, 2, 0);
 			vfov = 20.0;
+			break;
+
+		case 6:
+			world = cornell_box();
+			aspect_ratio = 1.0;
+			image_width = 600;
+			samples_per_pixel = 200;
+			background = color(0, 0, 0);
+			lookfrom = point3(278, 278, -800);
+			lookat = point3(278, 278, 0);
+			vfov = 40.0;
 			break;
 
 		default:
